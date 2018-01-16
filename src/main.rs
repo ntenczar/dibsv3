236a9b590c810b1b731311161e2f8da0ce744f7b
Nate Tenczar <ntenczar@gmail.com>
2018-01-16 18:45:25 -0500

    shrug

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,19 @@ extern crate serde_derive;
 
 extern crate rocket;
 extern crate rocket_contrib;
+extern crate rusqlite;
+extern crate time;
 
 mod queue;
 
 use queue::QueueRequest;
 use rocket_contrib::Json;
+use rocket::response::status;
 
 #[post("/queue", format = "application/json", data = "<queue_request>")]
-fn queue(queue_request: Json<QueueRequest>) -> &'static str {
-    println!("{:?}", queue_request);
-    return "watman";
+fn queue(queue_request: Json<QueueRequest>) -> status::Accepted<()> {
+    queue::enqueue(queue_request);
+    return status::Accepted::<()>(None);
 }
 
 fn main() {
--- src/queue.rs
+++ src/queue.rs
@@ -1,5 +1,15 @@
+extern crate rusqlite;
+extern crate time;
+
+use time::Timespec;
+use rusqlite::Connection;
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct QueueRequest {
     name: String,
     channel: String
+}
+
+pub fn enqueue(req: QueueRequest) {
+    println!("{:?}", req);
 }
